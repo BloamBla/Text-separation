4f9833da223d4fa1a133217eee2e2852ee2351d8
Stanislav Khromazhenkov <38785016+BloamBla@users.noreply.github.com>
2018-07-11 13:33:07 +0200

    Update mainJS.js

--- mainJS.js
+++ mainJS.js
@@ -1,8 +1,15 @@
 var myText = prompt("Введите текст.");
 var separator = prompt("Введите сепаратор.");
 
-function splitter(txt, sep){
-    return txt.split(sep).length;
+function splitter(txt, sep) {
+    var counter = 1;
+    for (var i = 0; i <= myText.length; i++) {
+        if (txt.charAt(i) === sep) {
+            counter += 1;
+        }
+    }
+
+    return counter;
 }
 
 alert("Количество фрагментов в вашем тексте - " + splitter(myText, separator) + ".\n"
