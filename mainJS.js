d28c49c7d57ad7d25cce1a5a2ab971ae92b5b593
Stanislav Khromazhenkov <38785016+BloamBla@users.noreply.github.com>
2018-07-11 18:52:43 +0200

    Update mainJS.js

--- mainJS.js
+++ mainJS.js
@@ -1,16 +1,33 @@
-var myText = prompt("Введите текст.");
-var separator = prompt("Введите сепаратор.");
-
-function splitter(txt, sep) {
-    var counter = 1;
-    for (var i = 0; i <= myText.length; i++) {
-        if (txt.charAt(i) === sep) {
-            counter += 1;
+var myText = prompt("Введите текст.", '');
+if (myText === null) {
+    alert("ещё увидимся ;)");
+}
+else {
+    if (myText === "") {
+        alert("Вы ничего не ввели!");
+    }
+    else {
+        var separator = prompt("Введите сепаратор.", '');
+        if (separator === null) {
+            alert("Так рано? До скорого.");
+        }
+        else {
+            function counter(txt, sep) {
+                var counter = 1;
+                if (sep === "") {
+                    return txt.length;
+                }
+                else {
+                    for (var i = 0; i <= txt.length; i++) {
+                        if (txt.charAt(i) === sep) {
+                            counter += 1;
+                        }
+                    }
+                    return counter;
+                }
+            }
+            alert("Количество фрагментов в вашем тексте - " + counter(myText, separator) + ".\n"
+                + "Разделителем был выбран символ '" + separator + "'.");
         }
     }
-
-    return counter;
 }
-
-alert("Количество фрагментов в вашем тексте - " + splitter(myText, separator) + ".\n"
-    + "Разделителем был выбран символ '" + separator + "'.");
