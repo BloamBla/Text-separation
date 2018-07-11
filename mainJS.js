a04b3565fed884ba0d9bdced8006f954433a0b76
Stanislav Khromazhenkov <38785016+BloamBla@users.noreply.github.com>
2018-07-11 10:50:21 +0200

    Update mainJS.js

--- mainJS.js
+++ mainJS.js
@@ -5,5 +5,5 @@ function splitter(txt, sep){
     return txt.split(sep).length;
 }
 
-alert("Количество фрагментов в вашем тексте " + splitter(myText, separator) + ".\n"
+alert("Количество фрагментов в вашем тексте - " + splitter(myText, separator) + ".\n"
     + "Разделителем был выбран символ '" + separator + "'.");
